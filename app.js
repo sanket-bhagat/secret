29791a1c3e3f2801713bd73d047be86a96621f40
Sanket Bhagat <sanketbhagat191@gmail.com>
2021-04-28 10:37:39 +0530

    Add procfile and update ports.

--- app.js
+++ app.js
@@ -165,8 +165,11 @@ app.post("/login",function(req,res){
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(3000,function(){
-    console.log("Sucessfully connected to port 3000.");
+app.listen(port,function(){
+    console.log("Server has started succesfully.");
 });
